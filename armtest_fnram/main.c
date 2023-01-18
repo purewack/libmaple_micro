8d54c96b7f7e23576c15766f1ac209e013c4dbf4
purewack <bobimaster15@gmail.com>
2023-01-18 23:38:49 +0000

    [dev] ramfn Hardfault :(

---
+++ armtest_fnram/main.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+
+const char* testString = "Hello";
+
+void delay_us(uint32_t us) {
+    us *= 3;
+
+    /* fudge for function call overhead  */
+    us--;
+    asm volatile("   mov r0, %[us]          \n\t"
+                 "1: subs r0, #1            \n\t"
+                 "   bhi 1b                 \n\t"
+                 :
+                 : [us] "r" (us)
+                 : "r0");
+}
+
+// void delay(uint32_t count)
+// {
+//   for(uint32_t i = 0 ; i < count ; i++);
+// }
+uint32_t v = 1;
+
+__attribute__((section(".cart"))) 
+void ramfn() {
+    *((uint32_t*)(0x40010C00 + 0xc)) = 0;
+} 
+
+
+int main(void){
+    
+	// // init();
+	// // while(1){
+	// // 	toggle();
+	// // 	delay_us(30000U);
+	// // };
+
+    //RCC->APB2ENR = enable GPIOA port
+    *((uint32_t*)(0x40021000 + 0x18)) = (uint32_t)(1<<3);
+        
+    //config gpioa 0 to be output PP
+    *((uint32_t*)(0x40010C00 + 0x04)) = (uint32_t)(0x11000000);
+
+    while(1){
+        *((uint32_t*)(0x40010C00 + 0xc)) = (uint16_t)(1<<14);
+        delay_us(200000);
+        ramfn();
+        delay_us(200000);
+    }
+	return 0;
+}
