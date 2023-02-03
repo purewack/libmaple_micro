833658443b9fce0f9568e4fe9ae57f7077e22c8b
purewack <bobimaster15@gmail.com>
2023-02-03 22:57:09 +0000

    [dev] Running foreign code loaded over USART !

--- armtest_fnram/cart/main.c
+++ armtest_fnram/cart/main.c
@@ -1,24 +1,25 @@
 #include "../libnumcalcium.h"
 
 void C_Reset(){
-    uint32_t sz_text = &_etext - &_stext;
-    uint32_t sz_data = &_edata - &_sdata;
-    uint32_t sz_bss  = &_ebss  - &_sbss;
-    uint32_t* dataSrc   = (uint32_t*)sz_text;
-    uint32_t* dataDest  = (uint32_t*)&_sdata;
-    uint32_t* bssDest   = (uint32_t*)&_sbss;
 
-    for(uint32_t i=0; i< sz_data; i++){
-        *dataDest++ = *dataSrc++;
-    }
-    for(uint32_t i=0; i< sz_bss; i++){
-        *bssDest++ = 0;
-    }
+    // uint32_t sz_text = &_etext - &_stext;
+    // uint32_t sz_data = &_edata - &_sdata;
+    // uint32_t sz_bss  = &_ebss  - &_sbss;
+    // uint32_t* dataSrc   = (uint32_t*)sz_text;
+    // uint32_t* dataDest  = (uint32_t*)&_sdata;
+    // uint32_t* bssDest   = (uint32_t*)&_sbss;
+
+    // for(uint32_t i=0; i< sz_data; i++){
+    //     *dataDest++ = *dataSrc++;
+    // }
+    // for(uint32_t i=0; i< sz_bss; i++){
+    //     *bssDest++ = 0;
+    // }
 
     while(1){
         *((uint32_t*)(GPIOB + 0xc)) = (uint16_t)(1<<14);
-        usleep(100000);
+        for(int i=0; i<200000; i++){}
         *((uint32_t*)(GPIOB + 0xc)) = 0;
-        usleep(100000);
+        for(int i=0; i<200000; i++){}
     };
 }
--- armtest_fnram/loader.c
+++ armtest_fnram/loader.c
@@ -1,9 +1,5 @@
 #include "libnumcalcium.h"
 
-
-__attribute__ ((section(".ctext")))
-void C_Reset(){}
-
 void post_libstatus(){
     USART_str("lib status: loaded\n");
 }
@@ -21,11 +17,16 @@ void usleep(uint32_t us) {
                  : "r0");
 }
 
+extern uint32_t _sctext;
+
 int main(void){
+
 
     USART_force_init();
     USART_str("\n[libnumcalcium cart loader]\n");
-    USART_str("Waiting for cart size... :");
+    USART_str("\n[using cart start @");
+    USART_hex((uint32_t)&_sctext);
+    USART_str("]\nWaiting for cart size... :");
     
     uint32_t cart_size = 0;
     cart_size =  (uint32_t)USART_get_char();
@@ -34,17 +35,30 @@ int main(void){
     cart_size |= (uint32_t)USART_get_char()<<24;
     USART_hex(cart_size);
 
-    char* data = (char*)0x20000000 + 4*1024;
+    char* data = (char*)&_sctext;
     USART_start_dma_rx(cart_size+1,(uint32_t)data);
     USART_str("\nWaiting on data... :");
     while(USART_dma_head(cart_size+1) != cart_size) usleep(1);
     USART_end_dma_rx();
 
-    USART_str("OK\n Waiting on start byte (any)\n");
+    USART_str("OK...Data dump:\n");
+    for(int i=0; i<cart_size; i++){
+        USART_hex(data[i]);
+        USART_char('\n');
+    }
+
+    USART_str("\n Waiting on start byte (any)\n");
     USART_get_char();
     
-    USART_str("Exec. C_Reset @ 0x20000400\n");
-    C_Reset();
+    // USART_str("Exec. C_Reset @");
+    // USART_hex((uint32_t)C_Reset_Loader);
+    // USART_str("\n");
+    // C_Reset_Loader();
+
+    void(*cloader)(void) = (void*)((uint32_t)&_sctext | 1);
+    (*cloader)();
+    
+    
     while(1);
 
 	return 0;
