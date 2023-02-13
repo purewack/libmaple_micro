08e486fe5283c0404b4a6e7b1537ca55e46019f9
purewack <bobimaster15@gmail.com>
2023-02-13 00:06:08 +0000

    [tests] testing libmaple

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <libnumcalcium.h>
 #include <libmaple/gpio.h>
 #include <libmaple/usart.h>
+#include <libmaple/adc.h>
+#include <libmaple/i2c.h>
 
 int main(void){
     usart_init(USART1);
@@ -15,11 +17,22 @@ int main(void){
     gpio_set_mode(GPIOA,10,GPIO_INPUT_FLOATING);
 
     gpio_init(GPIOB);
-    gpio_set_mode(GPIOB,13,GPIO_OUTPUT_PP);
+    gpio_set_mode(GPIOB,6,GPIO_OUTPUT_PP);
+    gpio_set_mode(GPIOB,7,GPIO_OUTPUT_PP);
+
+    
+    adc_init(ADC1);
+    adc_enable(ADC1);
+    adc_enable_single_swstart(ADC1);
+    gpio_set_mode(GPIOA,13,GPIO_INPUT_ANALOG);
 
     while(1){
-        usart_putstr(USART1,"[maple]\n");
-        gpio_toggle_bit(GPIOB,13);
+        int adcr = adc_read(ADC1,0);
+        usart_putstr(USART1,"[adc 0] ");
+        usart_putudec(USART1, adcr);
+        usart_putstr(USART1,"\n");
+        gpio_toggle_bit(GPIOB,6);
+        gpio_toggle_bit(GPIOB,7);
         usleep_8MHz(500000);
     }
 
